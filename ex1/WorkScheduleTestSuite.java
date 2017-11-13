fc3880b7a61b9cccbf1806241583c27ac8ac4ddd
Zombax <MrZombax@gmail.com>
2017-11-13 14:56:47 +0100

    Add automatic test runner

---
+++ ex1/WorkScheduleTestSuite.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({addWorkingPeriodTests.class, workingEmployeesTests.class})
+public class WorkScheduleTestSuite {
+
+}
