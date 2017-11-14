b33fa8f149b5c71bd904f89741a22680fec8a2a1
Joakim willard <joakimwillard@yahoo.se>
2017-11-14 17:36:25 +0100

    Added test for section.

---
+++ ex2/sectionTests.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class sectionTests {
+
+    private Set a;
+    private Set s;
+
+    @Before
+    public void setUp(){
+        a = new Set();
+        s = new Set();
+    }
+
+    @Test
+    public void sectionTest(){
+        a.insert(1);
+        a.insert(2);
+        a.insert(3);
+        s.insert(3);
+        s.insert(4);
+        s.insert(5);
+        a.section(s);
+        assertArrayEquals(new int[]{1}, a.toArray());
+    }
+}
