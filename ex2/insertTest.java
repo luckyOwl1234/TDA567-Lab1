5d435a1c4c53b997797d56422167ec572a209e7e
Zombax <MrZombax@gmail.com>
2017-11-14 18:53:20 +0100

    Add psuedoRandom test

--- ex2/insertTest.java
+++ ex2/insertTest.java
@@ -2,16 +2,19 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Random;
 import org.junit.Before;
 import org.junit.Test;
 
 public class insertTest {
 
     private Set set;
+    private Random rand;
 
     @Before
     public void setUp(){
         set = new Set();
+        rand = new Random();
     }
 
     @Test
@@ -28,5 +31,12 @@ public class insertTest {
         set.insert(2);
         set.insert(2);
         assertArrayEquals(new int[] {1,2,3}, set.toArray());
+    }
+
+    @Test
+    public void testInsertPsuedorandom() {
+        set.insert(2);
+        set.insert(1);
+        set.insert(3);
     }
 }
