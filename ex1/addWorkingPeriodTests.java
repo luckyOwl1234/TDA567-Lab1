c3069b9dde5d848641d802e4a274235f3311b0fc
Joakim willard <joakimwillard@yahoo.se>
2017-11-15 10:41:42 +0100

    Rewrote the tests in addWorkingPeriod in the asserts I used the hour objects required and array fields.

--- ex1/addWorkingPeriodTests.java
+++ ex1/addWorkingPeriodTests.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import org.junit.Assert;
@@ -14,31 +15,37 @@ public class addWorkingPeriodTests {
     }
 
     @Test
+    public void addWorkingPeriod_AddWorkerWhenNoneIsNeeded_DoNotAdd(){
+        work.addWorkingPeriod("John", 2,4);
+        assertEquals(0, work.readSchedule(2).requiredNumber);
+    }
+
+    @Test
     public void addWorkingPeriod_AddWorkerOnePerson_AddWorker() {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", 2, 4);
-        Assert.assertArrayEquals(new String[]{"John"}, work.workingEmployees(2, 4));
+        Assert.assertArrayEquals(new String[]{"John"}, work.readSchedule(2).workingEmployees);
     }
 
     @Test
     public void addWorkingPeriod_StartTimeBiggerThanEndTime_DoNotAddWorker() {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", 4, 2);
-        Assert.assertEquals(0, work.workingEmployees(2, 4).length);
+        Assert.assertEquals(0, work.readSchedule(2).workingEmployees.length);
     }
 
     @Test
     public void addWorkingPeriod_StartTimeLessThanZero_DoNotAddWorker() {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", -1, 4);
-        Assert.assertEquals(0, work.workingEmployees(2, 4).length);
+        Assert.assertEquals(0, work.readSchedule(2).workingEmployees.length);
     }
 
     @Test
     public void addWorkingPeriod_EndTimeHigherThanSize_DoNotAddWorker() {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", 2, 25);
-        Assert.assertEquals(0, work.workingEmployees(2, 4).length);
+        Assert.assertEquals(0, work.readSchedule(4).workingEmployees.length);
     }
 
     @Test
@@ -46,7 +53,7 @@ public class addWorkingPeriodTests {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", 2, 4);
         work.addWorkingPeriod("Mark", 2, 4);
-        Assert.assertEquals(1, work.workingEmployees(2, 4).length);
+        Assert.assertEquals(1, work.readSchedule(2).workingEmployees.length);
     }
 
     @Test
@@ -54,6 +61,6 @@ public class addWorkingPeriodTests {
         work.setRequiredNumber(3, 2, 4);
         work.addWorkingPeriod("John", 2, 4);
         work.addWorkingPeriod("Mark", 2, 4);
-        Assert.assertEquals(2, work.workingEmployees(2, 4).length);
+        Assert.assertEquals(2, work.readSchedule(3).workingEmployees.length);
     }
 }
