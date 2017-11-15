f6e5f4f1f24d4bb157f0fe49c5264f412041d79a
Joakim willard <joakimwillard@yahoo.se>
2017-11-15 12:01:21 +0100

    Added several tests in addWorkingPeriodTests

--- ex1/addWorkingPeriodTests.java
+++ ex1/addWorkingPeriodTests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,8 +17,13 @@ public class addWorkingPeriodTests {
 
     @Test
     public void addWorkingPeriod_AddWorkerWhenNoneIsNeeded_DoNotAdd(){
-        work.addWorkingPeriod("John", 2,4);
+        work.addWorkingPeriod("John", 2, 4);
         assertEquals(0, work.readSchedule(2).requiredNumber);
+    }
+
+    @Test
+    public void addWorkingPeriod_AddWorkerWhenNoneIsNeeded_ReturnFalse(){
+        assertFalse(work.addWorkingPeriod("John", 2, 4));
     }
 
     @Test
@@ -28,10 +34,23 @@ public class addWorkingPeriodTests {
     }
 
     @Test
+    public void addWorkingPeriod_AddWorkerOnePerson_ReturnTrue(){
+        work.setRequiredNumber(1, 2, 4);
+        assertTrue(work.addWorkingPeriod("John", 2, 4));
+    }
+
+    @Test
     public void addWorkingPeriod_StartTimeBiggerThanEndTime_DoNotAddWorker() {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", 4, 2);
-        Assert.assertEquals(0, work.readSchedule(2).workingEmployees.length);
+        Assert.assertEquals(0, work.readSchedule(4).workingEmployees.length);
+    }
+
+    //Bug addWorkingPeriod returns true even if it doesnt add a worker to the schedule
+    @Test
+    public void addWorkingPeriod_StartTimeBiggerThanEndTime_ReturnFalse(){
+        work.setRequiredNumber(1, 2, 4);
+        assertFalse(work.addWorkingPeriod("John", 4, 2));
     }
 
     @Test
@@ -42,11 +61,24 @@ public class addWorkingPeriodTests {
     }
 
     @Test
+    public void addWorkingPeriod_StartTimeLessThanZero_ReturnFalse() {
+        work.setRequiredNumber(1, 2, 4);
+        assertFalse(work.addWorkingPeriod("John", -1, 4));
+    }
+
+    @Test
     public void addWorkingPeriod_EndTimeHigherThanSize_DoNotAddWorker() {
         work.setRequiredNumber(1, 2, 4);
         work.addWorkingPeriod("John", 2, 25);
         Assert.assertEquals(0, work.readSchedule(4).workingEmployees.length);
     }
+
+    @Test
+    public void addWorkingPeriod_EndTimeHigherThanSize_ReturnFalse() {
+        work.setRequiredNumber(1, 2, 4);
+        assertFalse(work.addWorkingPeriod("John", 2, 25));
+    }
+
 
     @Test
     public void addWorkingPeriod_AddMoreWorkersThanNeeded_DoNotAddWorker() {
@@ -57,10 +89,69 @@ public class addWorkingPeriodTests {
     }
 
     @Test
-    public void addWorkingPeriod_AddWorkerFewerWorkersThanNeeded_AddWorker() {
+    public void addWorkingPeriod_AddMoreWorkersThanNeeded_ReturnFalse() {
+        work.setRequiredNumber(1, 2, 4);
+        work.addWorkingPeriod("John", 2, 4);
+        assertFalse(work.addWorkingPeriod("Mark", 2, 4));
+    }
+
+    @Test
+    public void addWorkingPeriod_AddFewerWorkersThanNeeded_AddWorker() {
         work.setRequiredNumber(3, 2, 4);
         work.addWorkingPeriod("John", 2, 4);
         work.addWorkingPeriod("Mark", 2, 4);
         Assert.assertEquals(2, work.readSchedule(3).workingEmployees.length);
+    }
+
+    @Test
+    public void addWorkingPeriod_AddFewerWorkersThanNeeded_ReturnTrue() {
+        work.setRequiredNumber(3, 2, 4);
+        work.addWorkingPeriod("John", 2, 4);
+        assertTrue(work.addWorkingPeriod("Mark", 2, 4));
+    }
+
+    @Test
+    public void addWorkingPeriod_AddWorkerOverBiggerIntervalThanNeeded_DoNotAddWorker(){
+        work.setRequiredNumber(1, 2, 4);
+        work.setRequiredNumber(1, 5, 6);
+        work.addWorkingPeriod("John", 2, 4);
+        work.addWorkingPeriod("Mark", 2, 6);
+        assertEquals(0, work.readSchedule(5).workingEmployees.length);
+    }
+
+    @Test
+    public void addWorkingPeriod_AddWorkerOverBiggerIntervalThanNeeded_ReturnFalse(){
+        work.setRequiredNumber(1, 2, 4);
+        work.setRequiredNumber(1, 5, 6);
+        work.addWorkingPeriod("John", 2, 4);
+        assertFalse(work.addWorkingPeriod("Mark", 2, 6));
+    }
+
+    @Test
+    public void addWorkingPeriod_StartTimeAndEndTimeIsTheSame_DoNotAddWorker(){
+        work.setRequiredNumber(1, 2,2);
+        work.addWorkingPeriod("John", 2,2);
+        assertEquals(1, work.readSchedule(2).workingEmployees.length);
+    }
+
+    @Test
+    public void addWorkingPeriod_StartTimeAndEndTimeIsTheSame_ReturnTrue(){
+        work.setRequiredNumber(1, 2,2);
+        assertTrue(work.addWorkingPeriod("John", 2,2));
+    }
+
+    @Test
+    public void addWorkingPeriod_AddSameWorkerMultipleTimes_DoNotAdd(){
+        work.setRequiredNumber(2, 2,4);
+        work.addWorkingPeriod("John", 2,4);
+        work.addWorkingPeriod("John", 2,4);
+        assertEquals(1, work.readSchedule(2).workingEmployees.length);
+    }
+
+    @Test
+    public void addWorkingPeriod_AddSameWorkerMultipleTimes_ReturnFalse(){
+        work.setRequiredNumber(2, 2,4);
+        work.addWorkingPeriod("John", 2,4);
+        assertFalse(work.addWorkingPeriod("John", 2,4));
     }
 }
--- ex1/workingEmployeesTests.java
+++ ex1/workingEmployeesTests.java
@@ -18,12 +18,12 @@ public class workingEmployeesTests {
         work.setRequiredNumber(2, 4,8);
         work.addWorkingPeriod("Linus", 4,6);
         work.addWorkingPeriod("Joakim", 4,5);
-        assertArrayEquals(new String[] {"Linus", "Joakim"}, work.workingEmployees(4,5));
+        assertArrayEquals(new String[] {"Linus", "Joakim"}, work.readSchedule(4).workingEmployees);
     }
 
     @Test
     public void workingEmployees_EmptySchedule_IsEmpty(){
-        assertEquals(0, work.workingEmployees(3,4).length);
+        assertEquals(0, work.readSchedule(3).workingEmployees.length);
     }
 
 }
