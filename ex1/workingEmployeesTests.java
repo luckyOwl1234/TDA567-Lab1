721d2ae17e7fbcfa22c4fee6985c235623b08e7c
Joakim willard <joakimwillard@yahoo.se>
2017-11-16 14:02:54 +0100

    Börjat skriva rapport för workingEmployees och lagt in lite fler tester i test filen

--- ex1/workingEmployeesTests.java
+++ ex1/workingEmployeesTests.java
@@ -14,16 +14,30 @@ public class workingEmployeesTests {
     }
 
     @Test
-    public void workingEmployees_AddToLimit_ContainCorrectWorkers(){
-        work.setRequiredNumber(2, 4,8);
-        work.addWorkingPeriod("Linus", 4,6);
-        work.addWorkingPeriod("Joakim", 4,5);
-        assertArrayEquals(new String[] {"Linus", "Joakim"}, work.readSchedule(4).workingEmployees);
+    public void workingEmployees_CheckingWorkingEmployeesArrayBelowZero_ReturnEmptyArray(){
+        work.setRequiredNumber(1,0,23);
+        work.addWorkingPeriod("John", 7,17);
+        assertEquals(0, work.workingEmployees(-1, -2).length);
     }
 
     @Test
-    public void workingEmployees_EmptySchedule_IsEmpty(){
-        assertEquals(0, work.readSchedule(3).workingEmployees.length);
+    public void workingEmployees_AddWorkersForStartTimeEqualToEndTime_ContainCorrectWorkers(){
+        work.setRequiredNumber(2, 5,5);
+        work.addWorkingPeriod("John", 5,5);
+        assertArrayEquals(work.readSchedule(5).workingEmployees, work.workingEmployees(5,5));
+    }
+
+    @Test
+    public void workingEmployees_AddWorkersToLimit_ContainCorrectWorkers(){
+        work.setRequiredNumber(2, 4,8);
+        work.addWorkingPeriod("John", 4,6);
+        work.addWorkingPeriod("Mark", 4,5);
+        assertArrayEquals(new String[] {"John", "Mark"}, work.workingEmployees(4,6));
+    }
+
+    @Test
+    public void workingEmployees_EmptySchedule_ReturnEmptyArray(){
+        assertEquals(0, work.workingEmployees(0,23).length);
     }
 
 }
